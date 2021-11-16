a29b45fdf32cc9c79491c12748464c5efa1bfae9
solera2 <rushtre1@gmail.com>
2021-11-16 15:10:02 -0500

    created basic layout in HTML/CSS

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # sky
+
+## Purpose
+a weather application to provide the forecast for a searched location. 
+
+## Built With
+* HTML
+* CSS
+* Bootstrap
+* JavaScript
+
+## Website 
+
+
+## Screenshot
---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+var apiKey = "2fe3148386e179649114eb803859fdb2"; 
+
+var location; 
+
+//var queryUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=" + apiKey;
+
+//fetch(queryUrl)
